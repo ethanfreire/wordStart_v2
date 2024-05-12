c691b6719f88b67d0a63dd4120281e5e44d667f8
ethanfreire <element55star@gmail.com>
2024-05-12 18:11:21 -0400

    added some backend changes, setup database connections, and test and successfully built

---
+++ backend/wordstart/src/main/java/com/ethanfreire/wordstart/WordstartApplication.java
@@ -0,0 +1,14 @@
+package com.ethanfreire.wordstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordstartApplication.class, args);
+	}
+
+}
+
---
+++ backend/wordstart/src/test/java/com/ethanfreire/wordstart/WordstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethanfreire.wordstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
